4d5b9ad2d37978216d3d5f0c630493df0207a36a
Milos-Savkovic <milos.savkovic@jsguru.io>
2018-12-07 13:59:23 +0100

    Updates

---
+++ precache-manifest.98e120fa8527792ff44cf1eeb8d0ab0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "b70101f27686529fe4b2",
+    "url": "/weather-react-app/static/js/main.b70101f2.chunk.js"
+  },
+  {
+    "revision": "7670d0a2ed5c0575c03a",
+    "url": "/weather-react-app/static/js/1.7670d0a2.chunk.js"
+  },
+  {
+    "revision": "b70101f27686529fe4b2",
+    "url": "/weather-react-app/static/css/main.3eb803a2.chunk.css"
+  },
+  {
+    "revision": "7670d0a2ed5c0575c03a",
+    "url": "/weather-react-app/static/css/1.ca218021.chunk.css"
+  },
+  {
+    "revision": "bcea6144d52609f055d103e8a562f9e4",
+    "url": "/weather-react-app/index.html"
+  }
+];
